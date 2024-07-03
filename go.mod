c605273cdf6206e53f6680ed70b8582e3a02f839
Taylor Monacelli <taylormonacelli@gmail.com>
2024-07-03 13:05:34 -0400

    Initial

--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.22.4
 
 require (
 	github.com/go-git/go-git/v5 v5.12.0
+	github.com/gphotosuploader/google-photos-api-client-go/v3 v3.0.5
 	github.com/magefile/mage v1.15.0
+	golang.org/x/oauth2 v0.21.0
 )
 
 require (
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	dario.cat/mergo v1.0.0 // indirect
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/ProtonMail/go-crypto v1.0.0 // indirect
@@ -17,6 +20,9 @@ require (
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.5.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/gphotosuploader/googlemirror v0.5.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.5 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/pjbgf/sha1cd v0.3.0 // indirect
@@ -28,5 +34,6 @@ require (
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/tools v0.13.0 // indirect
+	google.golang.org/api v0.167.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+
+	gphotos "github.com/gphotosuploader/google-photos-api-client-go/v3"
+	"github.com/gphotosuploader/google-photos-api-client-go/v3/media_items"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 func main() {
@@ -12,5 +19,78 @@ func main() {
 		os.Exit(0)
 	}
 
-	fmt.Println("Hello, World!")
+	ctx := context.Background()
+
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		fmt.Printf("error reading credentials.json: %v", err)
+		return
+	}
+
+	config, err := google.ConfigFromJSON(b, gphotos.PhotoslibraryReadonlyScope)
+	if err != nil {
+		fmt.Printf("error creating config from JSON: %v", err)
+		return
+	}
+
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Visit the URL for the auth dialog: %v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		fmt.Printf("error reading authorization code: %v", err)
+		return
+	}
+
+	token, err := config.Exchange(ctx, authCode)
+	if err != nil {
+		fmt.Printf("error exchanging authorization code for token: %v", err)
+		return
+	}
+
+	client := config.Client(ctx, token)
+	gphotosClient, err := gphotos.NewClient(client)
+	if err != nil {
+		fmt.Printf("error creating gphotos client: %v", err)
+		return
+	}
+
+	var allMediaItems []media_items.MediaItem
+	var nextPageToken string
+
+	for {
+		options := &media_items.PaginatedListOptions{
+			Limit:     100,
+			PageToken: nextPageToken,
+		}
+
+		mediaItems, token, err := gphotosClient.MediaItems.PaginatedList(ctx, options)
+		if err != nil {
+			fmt.Printf("error listing media items: %v", err)
+			return
+		}
+
+		allMediaItems = append(allMediaItems, mediaItems...)
+
+		if token == "" {
+			break
+		}
+		nextPageToken = token
+	}
+
+	file, err := os.Create("manifest.json")
+	if err != nil {
+		fmt.Printf("error creating manifest.json: %v", err)
+		return
+	}
+	defer file.Close()
+
+	enc := json.NewEncoder(file)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(allMediaItems); err != nil {
+		fmt.Printf("error encoding media items to JSON: %v", err)
+		return
+	}
+
+	fmt.Println("Manifest created successfully")
 }
